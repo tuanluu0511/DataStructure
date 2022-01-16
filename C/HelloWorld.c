e2498b0c198605e51c160f956331b5f994f75fc8
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-01-16 11:36:13 +0700

    Input and output: Prinf and Scanf - C Programming

---
+++ C/HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+int a,b,c;
+printf("Enter a: ");
+scanf("%d",&a);
+printf("Enter ab: ");
+scanf("%d",&b);
+c = a*b;
+printf("a=%d,b=%d,a*b=%d",a,b,c);
+return 0;
+}
+
