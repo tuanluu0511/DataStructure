2c4fca3555a9000f482fb66c14466880c56137ec
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-01-16 12:09:09 +0700

    Use Arithmetric Operators

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 int main() 
 {
-int a,b,c;
-printf("Enter a: ");
-scanf("%d",&a);
-printf("Enter ab: ");
-scanf("%d",&b);
-c = a*b;
-printf("a=%d,b=%d,a*b=%d",a,b,c);
-return 0;
-}
+  int a, b, diff, product,quotient,remainder;
+  printf("Enter a: ");
+  // &: scanf function to read the value from input, scanf store to the address value of a, then point to a address when receive the input and pass the value to a
+  scanf("%d",&a);
 
+  printf("Enter b: ");
+  scanf("%d",&b);
+
+// Pass operation as an argument
+  printf("a+b = %d\n", a+b);
+
+  diff = a-b;
+  printf("a-b = %d\n", diff);
+
+  product = a*b;
+  printf("a*b = %d\n", product);
+
+  quotient = a/b;
+  printf("a/b= %d\n", quotient);
+
+  remainder = a%b;
+  printf("a%%b = %d", remainder);
+  return 0;
+}
