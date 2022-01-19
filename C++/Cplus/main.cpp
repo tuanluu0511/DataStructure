425bafbd8a1137200ac95661d7e2416e36e33a8f
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-01-19 10:52:21 +0700

    Variable in C++

---
+++ C++/Cplus/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string friendName = "An";
+    string friendJob = "teacher";
+    int friendAge;
+    friendAge = 30;
+
+    cout << "I have an old friend named " <<friendName << endl;
+    cout << ""<<friendName<<" is a "<< friendJob << endl;
+    cout << "He's "<< friendAge <<" years old now" << endl;
+    cout << "But "<<friendName<<" does not look like "<<friendAge<<" !" << endl;
+    return 0;
+}
