70eaf7f9c2771f515d5779540e53717193499117
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-01-28 09:27:54 +0700

    Data Types, Basic String Operator

--- C++/Cplus/main.cpp
+++ C++/Cplus/main.cpp
@@ -4,6 +4,28 @@ using namespace std;
 
 int main()
 {
+    //Working with String:
+    string phrase = "HCMUT";
+    cout << phrase.length()<<endl; //Return 5
+    cout << phrase[0]<<endl; // return H
+    //Assign value: phrase[0]=B;
+    cout << phrase.find("MU",0)<<endl;//return 2
+    string phrasesub;
+    phrasesub = phrase.substr(2,3);
+    cout << phrasesub;
+    /*
+    //Data Types:
+    char first = 'A';
+    string hello = "Hello there!";
+    int a = 50;
+    double gpa = 2.3;
+    bool isMale = false;
+
+    cout << "Hi I'm Max" <<endl ;
+    return 0;
+
+
+    //Variable
     string friendName = "An";
     string friendJob = "teacher";
     int friendAge;
@@ -12,6 +34,5 @@ int main()
     cout << "I have an old friend named " <<friendName << endl;
     cout << ""<<friendName<<" is a "<< friendJob << endl;
     cout << "He's "<< friendAge <<" years old now" << endl;
-    cout << "But "<<friendName<<" does not look like "<<friendAge<<" !" << endl;
-    return 0;
+    cout << "But "<<friendName<<" does not look like "<<friendAge<<" !" << endl;*/
 }
