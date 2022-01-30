44b321192ae1c5b4bbddcf8001124a14f02138f2
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-01-30 10:31:19 +0700

    Pointer types, void pointer, pointer arithmetic

--- Pointer/C/Pointer/main.c
+++ Pointer/C/Pointer/main.c
@@ -3,13 +3,24 @@
 
 int main()
 {
+    int a = 1025;
+    int *p;
+    p=&a;
+    printf("%d bytes\n",sizeof(int)); //4 bytes
+    printf("%d,%d\n",p,*p); //address: 100, value: 1025 (00000000 00000000 00000100 00000001)
+    printf("%d,%d\n",p+1,*(p+1)); //address: 104, value: random
+    char *p0;
+    p0=(char*)p;//typecasting
+    printf("%d bytes\n",sizeof(char)); // 1 byte
+    printf("%d, %d\n",p0,*p0); //address: 100, value: 1(take value of smallest byte from p: 00000001)
+printf("%d, %d\n",p0+1,*(p0+1);
+    /*
     int a = 10;
     int* p = &a;
     //pointer arithmetic
     printf("%d\n",p); // 204
-    printf("%d\n",p+1); //208
+    printf("%d\n",p+1); //208 : size of integer is 4 bytes
 
-    /*
     int a;
     int *p;
     p = &a; // &a = address of a
